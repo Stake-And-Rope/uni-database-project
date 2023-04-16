ad6808c6d961448633749187bf34fb52b6077ebc
karastoyanov on ZorinOS <karastoqnov.alexandar@gmail.com>
2023-04-16 11:51:37 +0300

    PostgreSQL script connection established

--- postgres_conn.py
+++ postgres_conn.py
@@ -12,7 +12,7 @@ POSTGRES_USERNAME = input("Enter PostgreSQL username: ")
 POSTGRES_USER_PASSWD = getpass("Enter PostgreSQL user password: ")
 print("\n")
 
-def open_ssh_tunnel(verbose = False):
+def database_conn(verbose = False):
 
     # This function with create SSH Tunnel to the Linux Linode Server. 
     # Before using the connector disable the SSH Passphrase on your local machine with `ssh-keygen -p` and set new blank passphrase. <-- To be fixed with more reliable solution
@@ -30,18 +30,18 @@ def open_ssh_tunnel(verbose = False):
                 remote_bind_address = ('127.0.0.1', 5432)
                 )
         tunnel.start()
-        print("SSH connection established succsessfully!")
+        print(f"SSH connection established successfully!\n")
     except (Exception) as error:
-        print("SSH connection not established!")
+        print("SSH connection not established!\n")
 
         
     try:
         db_conn = psycopg2.connect(
                 user = str(POSTGRES_USERNAME),
                 password = str(POSTGRES_USER_PASSWD),
-                host = '127.0.0.1',
-                port = '5432',
-                database = 'uniproject'
+                host = "139.144.178.197",
+                port = "5432",
+                dbname = "uniproject"
                 )
         cursor = db_conn.cursor()
         print("PostgreSQL database version: ")
@@ -61,4 +61,4 @@ def close_sql_connection():
     # Close the connection to the PostgreSQL
     db_conn.close()
 
-open_ssh_tunnel()
+database_conn()
